a7df47aad3eef758cb17714c38358025bf3bd8ed
Lin <hl30@sp22-cs425-g03-01.cs.illinois.edu>
2022-02-01 21:44:13 -0600

    update logger

--- logger.py
+++ logger.py
@@ -8,52 +8,58 @@ import sys
 import logging as log
 from threading import Thread
 
+addr2node = dict()
+threads = []
 
-class Logger:
-    def __init__(self):
-        self.ndict = {}
-        self._set_args()
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bind(('172.22.94.8', self.port))
-        self.s.listen(1)
+def create_socket():
+    HOST = ''
+    PORT = 1234
+    try:
+        PORT = int(sys.argv[1])
+    except:
+        print("Please use ./logger <port>")
+        exit(1)
 
-    def addToIP(self,addr,nid):
-        self.ndict[addr] = nid
-    
-    def _set_args(self):
-        try:
-            self.port = int(sys.argv[1])
-        except:
-            print("Please use ./logger <port>")
-            exit(1)
-    
-    def TCP_connect(self):
-        self.conn, addr = self.s.accept()
-        data = self.conn.recv(1024).decode('utf-8').split(' ')
-        time_stamp = data[0]
-        node_name = data[1]
-        print(f'{time_stamp} - {node_name} connected')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    return s
 
-    def read(self):
+def TCP_connect(s):
+    conn, addr = s.accept()
+    addr = addr[0]
+    data = conn.recv(1024).decode('utf-8').split(' ')
+    time_stamp = data[0]
+    node_name = data[1]
+    if addr not in addr2node.keys():
+        addr2node[addr] = node_name
+
+    print(f'{time_stamp} - {node_name} connected')
+    return conn
+
+def read(conn):
+    with conn:
         while 1:
-            data = self.conn.recv(1024).decode('utf-8').split(' ')
+            data = conn.recv(1024).decode('utf-8').split(' ')
             if not len(data) == 3:
                 break 
             time_stamp = data[0] # e.g 1643485243.730725
             content = data[1] # e.g fca892488ee6f38ff20fde9720056dc9c454c680b5aef171036fe0468f81fc08
             node_name = data[2] # e.g node1
             print(f'{time_stamp} {node_name} {content}')            
-        self.s.close()
-        print(f'{time.time()} - {node_name} disconnected')   
-    
-# def assignThread(conn,addr,node_name):
+        conn.close()
+    print(f'{time.time()} - {node_name} disconnected')      
+
     
 if __name__ == '__main__':
-    logger = Logger()
+    s = create_socket()
+
     while True:
-        logger.TCP_connect()
-        _thread.start_new_thread(logger.read,())
-    
+        conn = TCP_connect(s)
+        handleRequest = threading.Thread(target=read,args=(conn,))
+        handleRequest.start()
+        threads.append(handleRequest)
+    #s.close() never going to use this line
     '''
     The main loop should be running and serving as logger.
     '''
