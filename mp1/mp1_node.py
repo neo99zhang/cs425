69a913f6cb0272bd10d18cae8ddacfb1c605f6cd
neo99zhang <Feiyu.17@intl.zju.edu.cn>
2022-03-03 00:52:14 -0600

    30 sleep for sender

--- mp1/mp1_node.py
+++ mp1/mp1_node.py
@@ -235,7 +235,7 @@ class node:
     def send(self):
         while not self.all_node_connected:
             time.sleep(1)
-        #time.sleep(10)
+        time.sleep(30)
         for line in sys.stdin: 
             msg = Message(line)
             # self.isis_mutex.acquire()    
