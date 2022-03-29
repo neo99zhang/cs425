90b6abcadaf39e3b3ae102633ea38b82b612d5f4
LinHangzheng <hangzheng.17@intl.zju.edu.cn>
2022-03-28 19:17:44 -0500

    mp2

--- mp2/src/raft/util.go
+++ mp2/src/raft/util.go
@@ -1,13 +1,56 @@
 package raft
 
-import "log"
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"time"
+)
 
 // Debugging
 const Debug = 0
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug > 0 {
-		log.Printf(format, a...)
-	}
-	return
+func DPrintf(format string, a ...interface{}) {
+    if Debug > 0 {
+        _, path, lineno, ok := runtime.Caller(1);
+        _, file := filepath.Split(path)
+
+        if (ok) {
+            t := time.Now();
+            a = append([]interface{} { t.Format("2006-01-02 15:04:05.00"), file, lineno }, a...);
+            fmt.Printf("%s [%s:%d] " + format + "\n", a...);
+        }
+    }
+}
+
+func Min(a int, b int) (int) {
+    if a < b {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+func Max(a int, b int) (int) {
+    if a > b {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+func Assert(flag bool, format string, a ...interface{}) {
+    if (!flag) {
+        _, path, lineno, ok := runtime.Caller(1);
+        _, file := filepath.Split(path)
+
+        if (ok) {
+            t := time.Now();
+            a = append([]interface{} { t.Format("2006-01-02 15:04:05.00"), file, lineno }, a...);
+            reason := fmt.Sprintf("%s [%s:%d] " + format + "\n", a...);
+            panic(reason);
+        } else {
+            panic("");
+        }
+    }
 }
